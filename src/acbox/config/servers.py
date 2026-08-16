8c65c23b6caad193d70a7f27d97b1bed14bbbfac
Antonio Cavallo <a.cavallo@cavallinux.eu>
2026-08-16 19:10:49 -0400

    change 3

--- src/acbox/config/servers.py
+++ src/acbox/config/servers.py
@@ -2,10 +2,10 @@ from pathlib import Path
 from sys import argv
 from typing import Any, Literal
 
-from ruamel.yaml import YAML
+from ruamel.yaml import YAML  # noqa: I001
 
 from acbox.config.types import InterfaceType  # noqa: I001
-from pydantic import BaseModel, ConfigDict, PrivateAttr
+from pydantic import BaseModel, ConfigDict, PrivateAttr  # noqa: I001
 
 
 class ConfigError(Exception):
